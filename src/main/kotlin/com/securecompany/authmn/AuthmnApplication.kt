cdbcfba0ff7d87d3dc6a073de420fa7ea3421cd5
Riccardo D'Aniello <rdaniello.external@epo.org>
2022-10-14 10:50:06 +0200

    Init

---
+++ src/main/kotlin/com/securecompany/authmn/AuthmnApplication.kt
@@ -0,0 +1,11 @@
+package com.securecompany.authmn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthmnApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthmnApplication>(*args)
+}
---
+++ src/test/kotlin/com/securecompany/authmn/AuthmnApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.securecompany.authmn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthmnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
